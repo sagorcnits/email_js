c799a7990eb2e4b2e2a11cbe9006bbd7df6d04a6
Sagor Hossain <130352065+sagorcnits@users.noreply.github.com>
2024-12-24 22:42:59 +0600

    some changes

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const contactForm = document.getElementById("contact-form");
+const contactForm = document.getElementById("contact-form")
 
-   emailjs.init("pVDExWkgwC5270NtH");
 
-  // handleSubmit form 
-  contactForm.addEventListener("submit", (e) => {
+emailjs.init("pVDExWkgwC5270Nt")
 
-    e.preventDefault();
-    // input value
-    const name = e.target.name.value;
-    const email = e.target.email.value;
-    const message = e.target.message.value;
 
-    const userData = { name, email, message };
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // form input value
+const name = e.target.name.value;
+const email = e.target.email.value;
+const message= e.target.message.value;
+const visitorData = {name, email, message};
+
 
 // send email
-    emailjs.send('service_qgoll5q', 'template_9s4fxmh', userData).then(
-      (response) => {
-        console.log('SUCCESS!', response.status, response.text);
-        contactForm.reset();
-      },
-      (error) => {
-        console.log('FAILED...', error);
-      },
-    );
+emailjs.send('service_ooceymc', 'template_3xp3s09', visitorData).then(
+  (response) => {
+    console.log('SUCCESS!', response.status, response.text);
+    alert("email successfully sended...!")
+    contactForm.reset()
+  },
+  (error) => {
+    console.log('FAILED...', error);
+  },
+);
 
-  });
+})
